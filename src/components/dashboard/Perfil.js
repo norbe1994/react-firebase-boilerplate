a450af5f84e56323d9701ed02099b37af685056b
Daniel <danieljust2395@gmail.com>
2019-06-25 14:18:19 -0500

    perfil

---
+++ src/components/dashboard/Perfil.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export class Perfil extends Component {
+    render() {
+        return (
+            <React.Fragment>
+            
+            
+            </React.Fragment>
+        )
+    }
+}
+
+export default Perfil
