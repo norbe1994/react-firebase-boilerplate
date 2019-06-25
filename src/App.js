79156b47a823d22009e06a48c5e03e26e6fabfda
Daniel <danieljust2395@gmail.com>
2019-06-25 15:10:00 -0500

    reformat

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 import React from 'react';
+import 'materialize-css/dist/css/materialize.css';
+import 'materialize-css/dist/js/materialize';
+import {BrowserRouter,Switch,Route} from 'react-router-dom';
+import Navbar from './components/layouts/Navbar';
+import SignIn from './components/auth/SignIn';
+import SignUp from './components/auth/SignUp';
+import Perfil  from './components/dashboard/Perfil';
 
 
 function App() {
   return (
+    <BrowserRouter>
     <div className="App">
-      <header className="App-header">
-      
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Navbar />
+    <Switch>
+    <Route exact path="/perfil" component={Perfil} />
+    <Route path="/signin" component={SignIn}/>
+    <Route path="/signup" component={SignUp}/>
+    </Switch>
     </div>
+    </BrowserRouter>
+    
   );
 }
 
