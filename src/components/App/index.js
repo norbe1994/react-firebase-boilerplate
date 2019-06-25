60602fd40c15bd3fb42ebb58c3ba70a7e000ce7d
Norberto <caceresnorbertoenrique@gmail.com>
2019-06-25 15:22:41 -0500

    crear estructura de carpetas

---
+++ src/components/App/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { BrowserRouter as Router } from 'react-router-dom'
+
+// componentes
+import Navigation from '../Navigation'
+
+const App = () => (
+	<div>
+		<Navigation />
+	</div>
+)
+
+export default App
---
+++ src/components/PasswordForget/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const PasswordForget = () => (
+	<div>
+		<h1>PasswordForger</h1>
+	</div>
+)
+
+export default PasswordForget
